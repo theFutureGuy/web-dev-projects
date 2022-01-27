ce091623835438f5454f5b27c05d0a213d7ff289
theFutureGuy <98336464+theFutureGuy@users.noreply.github.com>
2022-01-27 21:42:25 +0530

    Add files via upload

---
+++ html projects/app.js
@@ -0,0 +1,29 @@
+let count = 0;
+
+const value = document.querySelector('#value');
+const btns = document.querySelectorAll(".btn");
+
+btns.forEach(function (btn){
+  btn.addEventListener("click",function(e) {
+    const styles = e.currentTarget.classList;
+    if(styles.contains("decrease")){
+      count--;
+    }
+    else if (styles.contains("reset")) {
+      count=0;
+    }
+    else {
+      count++;
+    }
+    if(count===0)  {
+      value.style.color="black";
+    }
+    else if (count>0) {
+      value.style.color="green" ;
+    }
+    else{
+      value.style.color="red";
+    }
+    value.textContent = count;
+  });
+});
